f0794cada2aeeac046074339015305f086cadd47
Alex Gru <grusch.alex@gmail.com>
2016-04-24 11:17:01 +0200

    added starting points for widget (empty 4x1 with preview image for now)

---
+++ p3-stockhawk/src/main/java/com/sam_chordas/android/stockhawk/widget/StockWidgetProvider.java
@@ -0,0 +1,31 @@
+package com.sam_chordas.android.stockhawk.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.ui.MyStocksActivity;
+
+/**
+ * Created by Alex Gru on 24-Apr-16.
+ */
+public class StockWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+
+            Intent intent = new Intent(context, MyStocksActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
